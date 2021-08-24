e9ee2e4d49e10d8c67d9fd6e9ed293df6fb0e2c5
goelabhishek694 <goelabhishek694@gmail.com>
2021-08-24 20:00:13 +0530

    added upto moduleExports

---
+++ fileOrganizer/moduleExports/client.js
@@ -0,0 +1,6 @@
+let bObj=require('./owner');
+
+// console.log(b('How are you ?'));
+console.log(bObj.var);
+console.log(bObj.fooFnc('hello'));
+console.log(bObj.string);
---
+++ fileOrganizer/moduleExports/owner.js
@@ -0,0 +1,13 @@
+let str='Hakunama tata';
+let a=10;
+function foo(msg){
+    return 'Hi! i was made in owner.js '+msg
+};
+
+console.log('hello i was made in owner.js');
+
+module.exports={
+    var:a,
+    string:str,
+    fooFnc:foo
+};
