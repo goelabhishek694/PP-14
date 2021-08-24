e7da1120f76c845536936e50c906b3cb4bbe6988
goelabhishek694 <goelabhishek694@gmail.com>
2021-08-24 21:21:12 +0530

    added upto tree.js

--- fileOrganizer/Activity/commands/tree.js
+++ fileOrganizer/Activity/commands/tree.js
@@ -2,3 +2,24 @@
 // unhe print kra do 
 // prety simple 
 // // -> assignment recursion 
+
+let fs=require('fs');
+let path=require("path");
+
+"├──"
+function treeFn(srcPath){
+    let baseName=path.basename(srcPath);
+    console.log(baseName);
+    console.log("\n\t"+"└──");
+    let content=fs.readdirSync(srcPath);
+    let allEntities="";
+    for(let i=0;i<content.length;i++){
+        allEntities+='\n\t'+"├──"+content[i];
+        // console.log(content[i]);
+    }
+    console.log(allEntities);
+}
+
+module.exports={
+    treeFn:treeFn
+}
